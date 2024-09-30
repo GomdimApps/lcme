8fadc97454078f1df9f34834774ab2b616578691
GomdimApps <isackmota6565@gmail.com>
2024-09-30 16:27:27 -0300

    feat: improving bash function execution outputs

--- bash.go
+++ bash.go
@@ -2,13 +2,27 @@ package lcme
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
 )
 
 func Bash(command string) (string, error) {
 	cmd := exec.Command("bash", "-c", command)
+
 	var out bytes.Buffer
 	cmd.Stdout = &out
+
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
 	err := cmd.Run()
-	return out.String(), err
+	if err != nil {
+		return out.String(), fmt.Errorf("error executing command: %s\n%s", err, stderr.String())
+	}
+
+	if cmd.ProcessState.ExitCode() != 0 {
+		return out.String(), fmt.Errorf("command exited with non-zero status: %d", cmd.ProcessState.ExitCode())
+	}
+
+	return out.String(), nil
 }
